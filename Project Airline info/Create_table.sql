e7e1f9f7de382268fb5b5c505d2f5ffc99a79c38
47Levan <inimicos471@gmail.com>
2016-05-15 21:15:45 +0300

    Project_airline_info
    my first project

---
+++ Project Airline info/Create_table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE Flights
+(
+Arrival DATETIME,
+Departure DATETIME,
+Flight_Number Text,
+City_of_arrival Text,
+City_of_departure Text,
+Terimanl Text,
+Flight_Status Text,
+Gate Text,
+);
---
+++ Project Airline info/Project Airline info/YearCheck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Project_Airline_info
+{
+    class DateYearCheck :Exception
+    {
+        internal DateYearCheck()
+        {
+
+        }
+        internal DateYearCheck(string message):base(message)
+        {
+            message ="Year is out of range";
+        }
+        internal DateYearCheck(Exception inner,string message) : base(message,inner)
+        {
+            message = "Year is out of range";
+            inner = new Exception("Try enter birthday or flight year again");
+            
+        }
+    }
+}
